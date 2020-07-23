fcaad81b59afa774474e5b173fb761f5e6939ccb
enelesmai <xochitl.selene.fp@gmail.com>
2020-07-23 16:15:26 -0500

    added arrow function to index.js

--- src/index.js
+++ src/index.js
@@ -7,28 +7,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/style.css';
 import './assets/css/searchbox.css';
 
-function getInfo(search, mode) {
-  ApiController.getWeatherAsync(search, mode)
-    .then(data => DisplayController.displayWeather(data, mode));
+const getInfo = (search, mode) => {
+    ApiController.getWeatherAsync(search, mode)
+        .then(data => DisplayController.displayWeather(data, mode));
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const searchInput = document.getElementById('search_input');
-  const searchIcon = document.getElementById('search_icon');
-  const chkCelsius = document.getElementById('chkCelsius');
+    const searchInput = document.getElementById('search_input');
+    const searchIcon = document.getElementById('search_icon');
+    const chkCelsius = document.getElementById('chkCelsius');
 
-  // addevent onclick or enter
-  searchIcon.addEventListener('click', () => {
-    getInfo(searchInput.value, chkCelsius.checked ? 'C' : 'F');
-  });
+    // addevent onclick or enter
+    searchIcon.addEventListener('click', () => {
+        getInfo(searchInput.value, chkCelsius.checked ? 'C' : 'F');
+    });
 
-  searchInput.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-      getInfo(searchInput.value, chkCelsius.checked ? 'C' : 'F');
-    }
-  });
+    searchInput.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+            getInfo(searchInput.value, chkCelsius.checked ? 'C' : 'F');
+        }
+    });
 
-  chkCelsius.addEventListener('change', () => {
-    getInfo(searchInput.value, chkCelsius.checked ? 'C' : 'F');
-  });
+    chkCelsius.addEventListener('change', () => {
+        getInfo(searchInput.value, chkCelsius.checked ? 'C' : 'F');
+    });
 });
