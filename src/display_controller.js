e0d09031156b941c524fe0dc16c7146c14bd2cf7
enelesmai <xochitl.selene.fp@gmail.com>
2020-07-23 13:01:56 -0500

    imported background images

--- src/display_controller.js
+++ src/display_controller.js
@@ -1,41 +1,71 @@
+import imgThunderstorm from './assets/img/thunderstorm.jpg';
+import imgDrizzle from './assets/img/drizzle.jpg';
+import imgRain from './assets/img/rain.jpg';
+import imgSnow from './assets/img/snow.jpg';
+import imgClear from './assets/img/clear.jpg';
+import imgClouds from './assets/img/clouds.jpg';
+import imgAthmosphere from './assets/img/athmosphere.jpg';
 export const DisplayController = (() => {
-  const displayBackgroundWeather = (weather) => {
-    const img = document.getElementById('card-current');
-    const scenes = ['thunderstorm', 'drizzle', 'rain', 'snow', 'clear', 'clouds'];
-    let found = false;
-    scenes.forEach(element => {
-      if (weather.toLowerCase().includes(element)) {
-        img.style.backgroundImage = `url('/src/img/${element}.jpg')`;
-        found = true;
-      }
-    });
-    if (!found) {
-      img.style.backgroundImage = "url('/src/img/athmosphere.jpg')";
-    }
-  };
-  const displayWeather = (w, mode) => {
-    w.changeDisplayMode(mode);
-    document.getElementById('general').innerHTML = w.getGeneralDescripcion();
-    document.getElementById('temp').innerHTML = w.getDetails().temp;
-    document.getElementById('temp_max').innerHTML = w.getDetails().tempMax;
-    document.getElementById('temp_min').innerHTML = w.getDetails().tempMin;
-    document.getElementById('feels_like').innerHTML = w.getDetails().feelsLike;
-    document.getElementById('pressure').innerHTML = w.getDetails().pressure;
-    document.getElementById('humidity').innerHTML = w.getDetails().humidity;
-    document.getElementById('location-full-name').innerHTML = w.getFullCityName();
-    const img = document.getElementById('icon-weather');
-    img.src = w.getDetails().urlIcon;
-    const main = document.querySelector('main');
-    main.classList.remove('hide');
-    main.classList.remove('show-main');
-    main.classList.add('show-main');
-    displayBackgroundWeather(w.getGetMainWeather());
-    Array.from(document.getElementsByClassName('mode')).forEach(element => {
-      element.innerHTML = mode;
-    });
-  };
-  return {
-    displayWeather,
-  };
+    const displayBackgroundWeather = (weather) => {
+        const img = document.getElementById('card-current');
+        const scenes = [{
+                key: "thunderstorm",
+                value: imgThunderstorm
+            }, {
+                key: "drizzle",
+                value: imgDrizzle
+            },
+            {
+                key: "rain",
+                value: imgRain
+            },
+            {
+                key: "snow",
+                value: imgSnow
+            },
+            {
+                key: "clear",
+                value: imgClear
+            },
+            {
+                key: "clouds",
+                value: imgClouds
+            }
+        ];
+        let found = false;
+        scenes.forEach(item => {
+            if (weather.toLowerCase().includes(item.key)) {
+                img.style.backgroundImage = "url('" + item.value + "')";
+                found = true;
+            }
+        });
+        if (!found) {
+            img.style.backgroundImage = "url('" + imgAthmosphere + "')";
+        }
+    };
+    const displayWeather = (w, mode) => {
+        w.changeDisplayMode(mode);
+        document.getElementById('general').innerHTML = w.getGeneralDescripcion();
+        document.getElementById('temp').innerHTML = w.getDetails().temp;
+        document.getElementById('temp_max').innerHTML = w.getDetails().tempMax;
+        document.getElementById('temp_min').innerHTML = w.getDetails().tempMin;
+        document.getElementById('feels_like').innerHTML = w.getDetails().feelsLike;
+        document.getElementById('pressure').innerHTML = w.getDetails().pressure;
+        document.getElementById('humidity').innerHTML = w.getDetails().humidity;
+        document.getElementById('location-full-name').innerHTML = w.getFullCityName();
+        const img = document.getElementById('icon-weather');
+        img.src = w.getDetails().urlIcon;
+        const main = document.querySelector('main');
+        main.classList.remove('hide');
+        main.classList.remove('show-main');
+        main.classList.add('show-main');
+        displayBackgroundWeather(w.getGetMainWeather());
+        Array.from(document.getElementsByClassName('mode')).forEach(element => {
+            element.innerHTML = mode;
+        });
+    };
+    return {
+        displayWeather,
+    };
 })();
 export default DisplayController;
--- src/index.js
+++ src/index.js
@@ -4,31 +4,31 @@ import '@fortawesome/fontawesome-free/js/all';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/style.css';
-import './css/searchbox.css';
+import './assets/css/style.css';
+import './assets/css/searchbox.css';
 
 function getInfo(search, mode) {
-  ApiController.getWeatherAsync(search, mode)
-    .then(data => DisplayController.displayWeather(data, mode));
+    ApiController.getWeatherAsync(search, mode)
+        .then(data => DisplayController.displayWeather(data, mode));
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const searchInput = document.getElementById('search_input');
-  const searchIcon = document.getElementById('search_icon');
-  const chkCelsius = document.getElementById('chkCelsius');
+    const searchInput = document.getElementById('search_input');
+    const searchIcon = document.getElementById('search_icon');
+    const chkCelsius = document.getElementById('chkCelsius');
 
-  // addevent onclick or enter
-  searchIcon.addEventListener('click', () => {
-    getInfo(searchInput.value, chkCelsius.checked ? 'C' : 'F');
-  });
+    // addevent onclick or enter
+    searchIcon.addEventListener('click', () => {
+        getInfo(searchInput.value, chkCelsius.checked ? 'C' : 'F');
+    });
 
-  searchInput.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-      getInfo(searchInput.value, chkCelsius.checked ? 'C' : 'F');
-    }
-  });
+    searchInput.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+            getInfo(searchInput.value, chkCelsius.checked ? 'C' : 'F');
+        }
+    });
 
-  chkCelsius.addEventListener('change', () => {
-    getInfo(searchInput.value, chkCelsius.checked ? 'C' : 'F');
-  });
+    chkCelsius.addEventListener('change', () => {
+        getInfo(searchInput.value, chkCelsius.checked ? 'C' : 'F');
+    });
 });
